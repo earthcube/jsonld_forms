1849a2f5cfe838c56acb28d62e9c1de04ae6f8cc
David Valentine <david.valentine@gmail.com>
2022-02-11 13:47:45 -0800

    JsonForms Vuetify. expose JSON

--- src/components/Tools.vue
+++ src/components/Tools.vue
@@ -1,4 +1,5 @@
 <template>
+  <div>
   <json-forms
       :data="data"
       :schema="schema"
@@ -6,12 +7,24 @@
       :renderers="renderers"
       @change="onChange"
   />
+<v-expansion-panels>
+  <v-expansion-panel>
+    <v-expansion-panel-header>JSON</v-expansion-panel-header>
+    <v-expansion-panel-content>
+    <vue-json-pretty :path="'res'" :data="data" @click="handleClick"> </vue-json-pretty>
+    </v-expansion-panel-content>
+  </v-expansion-panel>
+</v-expansion-panels>
+  </div>
 </template>
 
 <script>
 import { JsonForms } from '@jsonforms/vue2';
 import { vuetifyRenderers } from '@jsonforms/vue2-vuetify';
 import {defineComponent} from "@vue/composition-api";
+import VueJsonPretty from 'vue-json-pretty';
+import 'vue-json-pretty/lib/styles.css';
+
 const schema= require('../assets/tools/ecrr_jsonschema_1_0.json');
 const uischema = require('../assets/tools/ecrr_1_0_uischema.json');
 const data = require('../assets/tools/ecrr_initialdata.json');
@@ -29,6 +42,7 @@ const tool = defineComponent({
   name: 'tools',
   components: {
     JsonForms,
+    VueJsonPretty
   },
   data() {
     return {
