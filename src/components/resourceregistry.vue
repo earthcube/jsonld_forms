69df2dc53e60ae3b05b39bea26faed28aa5bba5c
David Valentine <david.valentine@gmail.com>
2022-04-22 11:31:47 -0700

    Updated to ecrr v1.1

--- src/components/resourceregistry.vue
+++ src/components/resourceregistry.vue
@@ -37,9 +37,9 @@ import 'vue-json-pretty/lib/styles.css';
 
 
 import {default as saveFiles} from './controls/saveJson'
-import { default as schema, schemaWithEnum , flattenList} from '../schema/tools/ecrr_jsonschema_1_0' ;
+import { default as schema, schemaWithEnum , flattenList} from '../schema/tools/ecrr_jsonschema_1_1' ;
 
-import uischema from '../schema/tools/ecrr_1_0_uischema';
+import uischema from '../schema/tools/ecrr_1_1_uischema';
 // @ts-ignore
 const baseJsonLdObj = require('../assets/basefiles/tools/ecrrempty.json');
 
