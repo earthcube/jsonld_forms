88cf2e77f969ea3eced205622166d0b1006bb4f7
David Valentine <david.valentine@gmail.com>
2022-06-08 08:47:05 -0700

    fix router link for create resource description

--- src/components/startPage.vue
+++ src/components/startPage.vue
@@ -4,7 +4,7 @@
         <h2>Forms</h2>
         <v-list>
           <v-list-item>
-            <v-btn large color="primary" background-color="#18598B" class="create_resource_button" :to="/ecrr/">Create Resource Description</v-btn>
+            <v-btn large color="primary" background-color="#18598B" class="create_resource_button" to="/ecrr">Create Resource Description</v-btn>
 <!--            <router-link to="/ecrr" >Create Resource Description</router-link>-->
           </v-list-item>
         </v-list>
