5cc128b5e82023ee8dcb50f401a1b2f4cfdc0d25
Stephen Richard <513380+smrgeoinfo@users.noreply.github.com>
2022-06-23 07:25:26 -0700

    add vocabs, put loaders in separate files for each vocab

--- src/schema/tools/controlledFromGooglesheet/index.js
+++ src/schema/tools/controlledFromGooglesheet/index.js
@@ -1,129 +1,17 @@
-// /*
-// "creativeWork_type": {
-//       "type": "object",
-//       "properties": {
-//         "@type": {"const": "CreativeWork"},
-//         "name": {"type": "string"},
-//         "url": {"type": "string"}
-//       }
-//     },
-// */
-/*
-             {  "title": "License 1",
-               "const": {
-                 "@type": "CreativeWork",
-                 "name": "License 1",
-                 "url": "use if CreativeWork or URL; if use url, the value is a string"
-               }
-             }
-             */
-
-// const  toOneOf =  async  function(sheet, nameCol, urlCol) {
-//  let  results =  await Papa.parse(sheet, {
-//
-// 	header: true,
-// //     complete: function(results) {
-// //       console.log(results);
-// //       let propList = results.data.filter(o => o[nameCol]).map( o => {
-// //
-// //           return  {  "title": o[nameCol],
-// //                        "const": {
-// //                               "name": o[nameCol],
-// //                               "@type": "CreativeWork",
-// //                               "url": o[urlCol]
-// //                               }
-// //           }
-// //       })
-// //
-// //       return propList;
-// //       }
-// 	// rest of config ...
-//      })
-//      let propList = await results.data.filter(o => o[nameCol]).map( o => {
-//
-//           return  {  "title": o[nameCol],
-//                        "const": {
-//                               "name": o[nameCol],
-//                               "@type": "CreativeWork",
-//                               "url": o[urlCol]
-//                               }
-//           }
-//       })
-//
-//       return propList;
-// }
-
-//import  licenseSheet from  'csv-loader!../controlledFromGooglesheet/ECRR Controlled Vocabularies - Software Licenses.csv'
-import  licenseSheet from  'dsv-loader!../controlledFromGooglesheet/ECRR Controlled Vocabularies - Software Licenses.csv'
-const licenseOneOf =   function()  {
-    const nameCol = "Pruned for the form"
-    const urlCol ="URI"
-     const sheet = licenseSheet
-//    var  licenseSheet = require( 'csv-loader!../controlledFromGooglesheet/ECRR Controlled Vocabularies - Software Licenses.csv' )
-     let propList =  sheet.filter(o => o[nameCol]).map( o => {
-
-              return  {  "title": o[nameCol],
-                           "const": {
-                                  "name": o[nameCol],
-                                  "@type": "CreativeWork",
-                                  "url": o[urlCol]
-                                  }
-                        }
-               })
-     return {"oneOf":   propList }
-
- }
-
-export { licenseOneOf as licenseList}
-
-import  resourceTypeSheet from  'dsv-loader!../controlledFromGooglesheet/ECRR Controlled Vocabularies - Resource Types.csv'
-const resourceTypeOneOf =   function()  {
-    const nameCol = "Resource Type"
-    const urlCol ="URI"
-     const sheet = resourceTypeSheet
-//    var  licenseSheet = require( 'csv-loader!../controlledFromGooglesheet/ECRR Controlled Vocabularies - Software Licenses.csv' )
-     let propList =  sheet.filter(o => o[nameCol]).map( o => {
-
-              return  {  "title": o[nameCol],
-                           "const": [{
-                                  "name": o[nameCol],
-                                  "@type": "CreativeWork",
-                                  "url": o[urlCol]
-                                  } ]
-                        }
-               })
-     return {"oneOf":   propList }
-
- }
-export { resourceTypeOneOf as resourceTypeList}
-
-// oneOf.. this is part of an array. so each item is unique
-import  scienceDomain from  'dsv-loader!../controlledFromGooglesheet/sciencedomains.csv'
-const scienceDomainTypeOneOf =   function()  {
-    const nameCol = "SCIENCE DOMAIN"
-    const urlCol ="URI"
-     const sheet = scienceDomain
-//    var  licenseSheet = require( 'csv-loader!../controlledFromGooglesheet/ECRR Controlled Vocabularies - Software Licenses.csv' )
-     let propList =  sheet.filter(o => o[nameCol]).map( o => {
-
-              return  {  "title": o[nameCol],
-                           "const": {
-                                  "name": o[nameCol],
-                                  "@type": "DefinedTerm",
-                                  "identifier": o[urlCol]
-                                  }
-                        }
-               })
-     return {"oneOf":   propList }
-
- }
-export { scienceDomainTypeOneOf as scienceDomainList}
- //export default toOneOf
-
- //export default toOneOf
-//import {functionenum as functionenum, functionsuggestion as functionsuggestion} from './function_subfunction'
-export {functionenum as functionenum, functionsuggestion as functionsuggestion} from './function_subfunction'
+/*  This module gathers functions to read vocabularies from csv files in the controlledFromGooglesheet directory
+The vocabularies here are populated in instance documents as schema:definedTerm or schema:creativeWork
+Thus the values are JSON objects, not simple strings.
+*/
+export {fucntionenum as functionenum, functionsuggestion as functionsuggestion} from './function_category'
+export {subfunctionList as subfunctionList, subfunctionsuggestion as subfunctionsuggestion} from './function_subcategory'
+export {enumList as runtimeEnumList, suggestionList as runtimeSuggestionList} from './runtime'
+export {audienceList as audienceList} from './audience'
+export {resourceTypeOneOf as resourceTypeList} from './resourcetypes'
+export {specificationOneOf as specificationOneOf} from './specifications'
+export {semanticResourceOneOf as semanticResourceOneOf} from './semanticresources'
+export {communicationOneOf as communicationOneOf } from './communication'
+export {scienceDomainTypeOneOf as scienceDomainList} from './sciencedomains'
 export {maturityOneOf as maturityOneOf} from './maturity'
 export {lifetimeOneOf as lifetimeOneOf} from './lifetime'
-export {audienceOneOf as audienceOneOf} from './audience'
-export { enumList as runtimeEnumList, suggestionList as runtimeSuggestionList} from './runtime'
+export {licenseOneOf as licenseList} from './softwarelicenses'
+
