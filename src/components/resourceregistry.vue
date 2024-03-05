76aa833e7affe4b54a306d5cb10087055b435122
David Valentine <david.valentine@gmail.com>
2024-03-04 22:08:03 -0800

    Resource Back to being a menu

--- src/components/resourceregistry.vue
+++ src/components/resourceregistry.vue
@@ -176,7 +176,7 @@ export default defineComponent({
     }
 
   },
-  beforeCreate() {
+  beforeMount() {
     // schemaWithEnum().then(s =>{
     //       console.log(s)
     //       this.schema = s
--- src/schema/tools/controlledFromGooglesheet/resourcetypes.js
+++ src/schema/tools/controlledFromGooglesheet/resourcetypes.js
@@ -15,6 +15,6 @@ export const resourceTypes =   function()  {
             }
         }
     })
-    return {"anyOf":   propList }
+    return {"oneOf":   propList }
 }
 export { resourceTypes as resourceTypeList}
